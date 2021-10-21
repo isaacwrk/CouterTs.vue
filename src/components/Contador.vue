a1e4756160786680b0c8c1d8037036c3bfeb580a
isaacwrk <isaacsouzawrk@gmail.com>
2021-10-21 10:47:09 -0300

    initial commit

---
+++ src/components/Contador.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+<h1>Contador</h1>
+<h2>{{valor}}</h2>
+</div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const contador = defineComponent({
+    data(){
+        return{
+            valor:0
+        }
+    },
+    methods:{
+        setValor(delta:number){
+            this.valor += delta
+        }
+    }
+});
+export default contador;
+
+</script>
+
+<style>
+
+</style>
